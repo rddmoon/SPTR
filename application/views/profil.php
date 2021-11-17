926dd552661c8afc12100f6ef4a717c9cc8d4185
APTX-4869 <61036923+S1A4C1D2@users.noreply.github.com>
2021-11-18 02:18:21 +0700

    Create profil.php

---
+++ application/views/profil.php
@@ -0,0 +1,23 @@
+<section class="section">
+    <div class="section-header">
+        <h1>Profil</h1>
+    </div>
+    <div class="row">
+        <div class="col-12 col-offset-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Profil</h4>
+                </div>
+                <div class="card-body">
+                    <div class="text-right">
+                        <a href="<?=site_url('user/edit_profil/'.$current_user->id)?>" class="btn btn-warning">
+                        <i class="fa fa-edit"></i> Edit</a>
+                    </div>
+                    <p>Username&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <?=$current_user->username?></p>
+                    <p>Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <?=$current_user->nama?></p>
+                    <p>Role&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <?=$current_user->role?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
