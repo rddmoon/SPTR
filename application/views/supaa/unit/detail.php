a22c04c37247a809d6842552ae900c1de6c44a6d
APTX-4869 <61036923+S1A4C1D2@users.noreply.github.com>
2021-12-01 21:20:32 +0700

    Update detail.php

--- application/views/supaa/unit/detail.php
+++ application/views/supaa/unit/detail.php
@@ -19,7 +19,7 @@
                       <p><b>Status:</b>&nbsp;&nbsp;&nbsp;&nbsp;<span class="badge badge-danger"><?=$unit->status?></span></p>
                     <?php } ?>
                   </div>
-                    <p>Nama Perumahan&nbsp;&nbsp;: <?=$perumahan->nama?></p>
+                    <p>Nama Perumahan&nbsp;&nbsp;: <?=$unit->nama_perumahan?></p>
                     <p>Cluster&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <?=$unit->cluster?></p>
                     <p>Blok&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <?=$unit->blok?></p>
                     <p>Tipe Rumah&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <?=$unit->tipe_rumah?></p>
